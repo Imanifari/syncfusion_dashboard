ca9f648b5fcdc4392bc891c18ec32ef03b8460c0
Imanifari <imaniahweb@gmail.com>
2025-09-18 10:34:12 -1100

    feat: i added the navbar with all it logics

--- src/App.js
+++ src/App.js
@@ -43,15 +43,17 @@ const App = () => {
             </TooltipComponent>
           </div>
           {/* sidebar */}
-          {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              <Sidebar />
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar />{" "}
-            </div>
-          )}
+          <div>
+            {activeMenu ? (
+              <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+                <Sidebar />
+              </div>
+            ) : (
+              <div className="w-0 dark:bg-secondary-dark-bg">
+                <Sidebar />{" "}
+              </div>
+            )}
+          </div>
           {/* navbar */}
           <div
             className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
@@ -59,6 +61,9 @@ const App = () => {
             } `}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+            <div>
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
