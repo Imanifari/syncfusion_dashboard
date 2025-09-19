1ec50c4a1805080c08e4b7469b2e93ee1908c124
Imanifari <imaniahweb@gmail.com>
2025-09-19 10:14:39 -1100

    feat: just add the home page for the dashboard up to the butdget section in the page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ color, bgColor, text, size, borderRadius }) => {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
