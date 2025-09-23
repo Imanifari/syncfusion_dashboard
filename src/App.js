c48cd7ee782b12585297243c1e7bb9559f4b295e
Imanifari <imaniahweb@gmail.com>
2025-09-23 06:12:28 -1100

    feat: update Syncfusion packages and enhance ColorPicker and Editor components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { Navbar, Footer, Sidebar, Themesetting } from "./components";
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
   Ecommerce,
   Orders,
